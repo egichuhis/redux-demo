a4decc8b62a1802ac4ff96f543b39c2f30cf9d1a
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-09-26 21:51:29 +0300

    legacy redux

--- index.js
+++ index.js
@@ -1,52 +1,88 @@
 const redux = require('redux');
-const createStore = redux.createStore; 
+const createStore = redux.createStore;
 
 const CAKE_ORDERED = 'CAKE_ORDERED';
-const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED';
+const ICECREAM_ORDERED = 'ICECREAM_ORDERED';
+const ICECREAM_RESTOCKED = 'ICECREAM_RESTOCKED';
 
-function orderCake() {
+// Action creators
+function orderCake(qty = 1) {
   return {
     type: CAKE_ORDERED,
-    payload: 1,
-  } 
+    payload: qty,
+  };
 }
 
 function restockCake(qty = 1) {
   return {
     type: CAKE_RESTOCKED,
     payload: qty,
-  }
+  };
 }
 
+function orderIceCream(qty = 1) {
+  return {
+    type: ICECREAM_ORDERED,
+    payload: qty,
+  };
+}
+
+function restockIceCream(qty = 1) {
+  return {
+    type: ICECREAM_RESTOCKED,
+    payload: qty,
+  };
+}
+
+// Initial state
 const initialState = {
   numOfCakes: 10,
-}
+  numOfIceCreams: 20,
+};
 
+// Reducer
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case CAKE_ORDERED:
       return {
-        numOfCakes: state.numOfCakes - 1
-      }
+        ...state,
+        numOfCakes: state.numOfCakes - action.payload,
+      };
     case CAKE_RESTOCKED:
       return {
         ...state,
-        numOfCakes: state.numOfCakes += action.payload,
-      }
+        numOfCakes: state.numOfCakes + action.payload,
+      };
+    case ICECREAM_ORDERED:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams - action.payload,
+      };
+    case ICECREAM_RESTOCKED:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams + action.payload,
+      };
     default:
-      return state
+      return state;
   }
-}
+};
 
 const store = createStore(reducer);
 console.log('Initial state', store.getState());
 
 const unsubscribe = store.subscribe(() =>
-  console.log('Updated state', store.getState()));
+  console.log('Updated state', store.getState())
+);
 
+// Dispatch actions
 store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(restockCake(3));
+store.dispatch(orderIceCream());
+store.dispatch(orderIceCream());
+store.dispatch(restockIceCream());
 
 unsubscribe();
