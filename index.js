d31a1066611bce3bff7014630a5c89207fe089df
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-09-26 20:32:46 +0300

    sads

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const redux = require('redux');
 const createStore = redux.createStore; 
 
-const CAKE_ORDERED = 'CAKE_ORDERED'
+const CAKE_ORDERED = 'CAKE_ORDERED';
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
 
 function orderCake() {
   return {
     type: CAKE_ORDERED,
-    quantity: 1,
+    payload: 1,
   } 
+}
+
+function restockCake(qty = 1) {
+  return {
+    type: CAKE_RESTOCKED,
+    payload: qty,
+  }
 }
 
 const initialState = {
@@ -19,6 +27,11 @@ const reducer = (state = initialState, action) => {
     case CAKE_ORDERED:
       return {
         numOfCakes: state.numOfCakes - 1
+      }
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes += action.payload,
       }
     default:
       return state
@@ -34,5 +47,6 @@ const unsubscribe = store.subscribe(() =>
 store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(orderCake());
+store.dispatch(restockCake(3));
 
 unsubscribe();
